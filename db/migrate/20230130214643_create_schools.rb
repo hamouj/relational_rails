3ed4eb99ad9ed65386624adb9ee61450e32804d0
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-30 14:14:04 -0800

    Feat: create schools and teachers databases

---
+++ db/migrate/20230130214643_create_schools.rb
@@ -0,0 +1,11 @@
+class CreateSchools < ActiveRecord::Migration[5.2]
+  def change
+    create_table :schools do |t|
+      t.string :name
+      t.integer :open_year
+      t.fully_staffed :boolean
+      t.integer :open_year
+      t.timestamps
+    end
+  end
+end
