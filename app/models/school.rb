3783a856122a89b5837b9e9c838920b563dcba3a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-31 20:30:02 -0800

    Test: create and pass model tests for #show_teachers

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ApplicationRecord
+  has_many :teachers
+
+  def show_teachers
+    @teachers = Teacher.where(school_id: id)
+  end
 end
--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe School, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe School, type: :model do
+  describe '#show_teachers' do
+    it 'lists all teachers from a specific school' do
+      school = School.create!(name: 'Frias', open_year: 2003, fully_staffed: false, created_at: DateTime.now, updated_at: DateTime.now)
+      school2 = School.create!(name: 'Robison', open_year: 1973, fully_staffed: false, created_at: DateTime.now, updated_at: DateTime.now)
+
+      teacher = school.teachers.create!(name: 'Mrs.Vicario', years_at_school: 7, bilingual: false, created_at: DateTime.now, updated_at: DateTime.now)
+      teacher2 = school.teachers.create!(name: 'Mrs. Auch', years_at_school: 10, bilingual: false, created_at: DateTime.now, updated_at: DateTime.now)
+      
+      expect(school.show_teachers).to eq([teacher, teacher2])
+    end
+
+    it 'only lists teachers for a specific school' do
+      school = School.create!(name: 'Frias', open_year: 2003, fully_staffed: false, created_at: DateTime.now, updated_at: DateTime.now)
+      school2 = School.create!(name: 'Robison', open_year: 1973, fully_staffed: false, created_at: DateTime.now, updated_at: DateTime.now)
+
+      teacher = school.teachers.create!(name: 'Mrs.Vicario', years_at_school: 7, bilingual: false, created_at: DateTime.now, updated_at: DateTime.now)
+      teacher2 = school.teachers.create!(name: 'Mrs. Auch', years_at_school: 10, bilingual: false, created_at: DateTime.now, updated_at: DateTime.now)
+      teacher3 = school2.teachers.create!(name: 'Mrs. Hall', years_at_school: 15, bilingual: true, created_at: DateTime.now, updated_at: DateTime.now)
+
+      expect(school.show_teachers).to eq([teacher, teacher2])
+    end
+  end
 end
